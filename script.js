e49b4629bde6c4d66249890a10790898aa15fa28
carissaalani <130004423+carissaalani@users.noreply.github.com>
2023-04-28 23:41:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 /*Javascript*/
-
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("addCalories").addEventListener("click", function () {
+        console.log();
