2e266472d03ffc3c381b8e8bab211f22b9181d4f
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-04-29 17:49:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("addCalories").addEventListener("click", function () {
         console.log();
+
+
+        let foodArray = [
+	{
+		name: 'Banana',
+		calories: 105
+	},
+	{
+		name: 'Hot Dog',
+		calories: 565
+	}
+];
+
+const form = document.querySelector('form');
+const foodNameInput = document.querySelector('#foodName');
+const caloriesInput = document.querySelector('#calories');
+const totalCaloriesOutput = document.querySelector('#totalCalories');
+const foodListOutput = document.querySelector('#foodList');
