1b8cf6966a40c465468b673d80b5e97c14124a01
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-04-29 23:49:07 -0700

    Updated the JS

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 /*Javascript*/
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.getElementById("addCalories").addEventListener("click", function () {
-        console.log(foodArray);
-        { }
-        const form = document.getElementById('form');
-        const foodNameInput = document.getElementById('foodName');
-        const caloriesInput = document.getElementById('calories');
-        const totalCaloriesOutput = document.getElementById('totalCalories');
-        const foodListOutput = document.getElementById('foodList');	    
+// Define Food class
+class Food {
+    constructor(name, calories) {
+        this.name = name;
+        this.calories = parseInt(calories);
+    }
+}
+
+// Initialize variables
+let foodLog = [];
+
+// Get DOM elements
+const form = document.querySelector('form');
+const foodNameInput = document.querySelector('#foodName');
+const caloriesInput = document.querySelector('#calories');
+const totalCaloriesOutput = document.querySelector('#totalCalories');
+const foodListOutput = document.querySelector('#foodList');
+
+// Define event listeners
+addFoodBtn.addEventListener('click', addFood);
+
+// Define event handler functions
+function addFood() {
+    // Get values from input fields
+    const foodName = foodNameInput.value;
+    const calories = caloriesInput.value;
+
+    // Create new Food object and add to foodLog array
+    const newFood = new Food(foodName, calories);
+    foodLog.push(newFood);
+
+    // Update food log display
+    updateFoodLogDisplay();
+
+    // Update total calories display
+    updateTotalCaloriesDisplay();
+
+    // Reset input fields
+    food
+}
