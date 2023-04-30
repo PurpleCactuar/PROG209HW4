854e7645cf37f9d252470100100a706f2a514e7f
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-04-30 11:51:55 -0700

    Added working JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Javascript*/
+/*Not working
 // Define Food class
 class Food {
     constructor(name, calories) {
@@ -38,4 +38,45 @@ function addFood() {
 
     // Reset input fields
     food
+}
+*/
+
+/*Working Java*/
+// Define the Food constructor
+function Food(name, calories) {
+  this.name = name;
+  this.calories = parseInt(calories);
+}
+
+// Initialize the variables and event listener
+var foodList = [];
+var totalCalories = 0;
+var form = document.querySelector('form');
+var foodNameInput = document.querySelector('#foodName');
+var caloriesInput = document.querySelector('#calories');
+var foodListDiv = document.querySelector('#foodList');
+var totalCaloriesDiv = document.querySelector('#totalCalories');
+form.addEventListener('submit', addFood);
+
+// Define the addFood function
+function addFood(event) {
+  event.preventDefault(); // Prevent the form from submitting
+  var foodName = foodNameInput.value;
+  var calories = caloriesInput.value;
+  var food = new Food(foodName, calories);
+  foodList.push(food);
+  totalCalories += food.calories;
+  foodListDiv.textContent = ''; // Clear the food list
+  // Display the updated food list in the console
+  for (var i = 0; i < foodList.length; i++) {
+    console.log(foodList[i].name + ': ' + foodList[i].calories + ' calories');
+    var foodDiv = document.createElement('div');
+    foodDiv.textContent = foodList[i].name + ': ' + foodList[i].calories + ' calories';
+    foodListDiv.appendChild(foodDiv);
+  }
+  // Display the total number of calories consumed
+  totalCaloriesDiv.textContent = 'Total calories consumed: ' + totalCalories;
+  // Clear the input fields
+  foodNameInput.value = '';
+  caloriesInput.value = '';
 }
